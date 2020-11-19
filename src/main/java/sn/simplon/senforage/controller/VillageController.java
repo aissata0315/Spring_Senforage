e9d5d79645dd75f8ddba9ca09f557ca07191cca4
aissata0315 <aissata0315@gmail.com>
2020-11-19 16:11:06 +0000

    enregistrement village

--- src/main/java/sn/simplon/senforage/controller/VillageController.java
+++ src/main/java/sn/simplon/senforage/controller/VillageController.java
@@ -3,6 +3,7 @@ package sn.simplon.senforage.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 import sn.simplon.senforage.dao.IVillage;
 import sn.simplon.senforage.entities.Village;
@@ -20,8 +21,23 @@ public class VillageController {
 
         List<Village> villages = villagedao.findAll();
         return new ModelAndView("village/liste", "liste_villages", villages);
+    }
 
+    @RequestMapping(value="Village/add", method = RequestMethod.POST)
+    public String add(String nom){
+        ModelAndView modelAndView = new ModelAndView();
+        Village village = new Village();
+        village.setNom(nom);
 
+        try {
+            villagedao.save(village);
+            villagedao.flush();
+            //modelAndView.addObject("result",new String("Données ajoutees"));
+        }catch (Exception e){
+            //modelAndView.addObject("result",new String("Données non ajoutees"));
+        }
+       // modelAndView.addObject(new String("village/liste"));
+        return "redirect:/Village/liste";
 
     }
 }
